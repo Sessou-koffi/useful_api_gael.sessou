725de8715c310474ddc08fd4b2672aaba20e00d8
Sessou-koffi <gaelsessou857@gmail.com>
2025-10-16 17:09:15 +0100

    feat/module_0. Added authentification feature

---
+++ Evaluation_first/app/Models/Module.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Module extends Model
+{
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_modules')
+                    ->withPivot('active')
+                    ->withTimestamps();
+    }
+}
+
